476cff966336a08a3abb16310e8e4ec1e40f230c
No5972 <wujiuqier@foxmail.com>
2021-04-25 01:35:07 +0800

    V3.1

--- scenes/levelup.js
+++ scenes/levelup.js
@@ -1,19 +1,12 @@
-add([
-  pos(width() / 2, height() / 2),
-  text("Stage Clear\nPress Enter to Continue")
-]);
+    add([
+      pos(width() / 2, height() / 2),
+      text("Stage Clear\nPress Enter to Continue")
+    ]);
 
-keyPress("enter", () => {
-  bgm.pause();
-  bgm.src = "./sounds/th06_04.mp3";
-  bgm.load();
-  bgm.play();
-  go("level" + args.nextLevel, {
-    power: args.power,
-    life: args.life,
-    bomb: args.bomb,
-    initScore: args.initScore,
-    graze: args.graze,
-    itemCount: args.itemCount
-  });
-})
+    keyPress("enter", () => {
+      bgm.pause();
+      bgm.src = "./sounds/th06_04.mp3";
+      bgm.load();
+      bgm.play();
+      go("level2", {});
+    })
